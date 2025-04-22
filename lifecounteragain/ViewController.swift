30329c8f0502de72d707ff6042c021b30b3c2244
Cleo <121153199+cleoreyes@users.noreply.github.com>
2025-04-21 19:13:36 -0700

    Initial Commit

---
+++ lifecounteragain/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lifecounteragain
+//
+//  Created by Cleo Reyes on 4/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ lifecounteragainTests/lifecounteragainTests.swift
@@ -0,0 +1,17 @@
+//
+//  lifecounteragainTests.swift
+//  lifecounteragainTests
+//
+//  Created by Cleo Reyes on 4/21/25.
+//
+
+import Testing
+@testable import lifecounteragain
+
+struct lifecounteragainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
